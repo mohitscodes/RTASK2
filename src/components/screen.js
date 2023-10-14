e5eafa1ecf1c22af35e8e6d1e877e0e6b6501cfb
Mohit <mohit167@Mohits-MacBook-Air.local>
2023-10-15 00:55:23 +0530

    first commit

--- src/components/screen.js
+++ src/components/screen.js
@@ -1,11 +1,8 @@
-import { Textfit } from "react-textfit";
-import "./screen.css"
+import "./screen.css";
 
 const Screen = ({ value }) => {
   return (
-    <Textfit className="screen" mode="single" max={70}>
-      {value}
-    </Textfit>
+    <div className="screen">{value}</div>
   );
 };
 
