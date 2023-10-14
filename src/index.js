c895316cfd5a423b087a6be470b956ad093d69d3
Mohit <mohit167@Mohits-MacBook-Air.local>
2023-10-15 00:34:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
